b38bfa490b52a34de6f74723f1d4df45104b8219
Felix Stüber <felix.stueber@webit.de>
2022-12-22 10:15:07 +0100

    tiny commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,17 @@ Rails.application.routes.draw do
   # Get /about
   get "about", to: "about#index"
 
+  get "password", to: "passwords#edit"
+  patch "password", to: "passwords#update"
+
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
+
+  delete "logout", to: "sessions#destroy"
+
   root to: "main#index"
 end
